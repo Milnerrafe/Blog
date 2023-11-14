f2c5e466239c641b34cab14942bff17ae96c959d
milnerrafe <milnerrafe@gmail.com>
2023-11-14 12:12:04 +0000

    new 404

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,22 @@
 import Head from 'next/head';
-import utilStyles from '../../styles/utils.module.css';
-import Layout from '../../components/layout';
+import Image from 'next/image';
+import styles from '../styles/404.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
+const name = 'Rafe Milner';
+export const siteTitle = 'Rafe Media Blog';
 
-export default function Custom404()  { return(
-  <Layout>
+export default function Custom404() {
+  return (
+    <div className={styles.container}>
+
     <Head> <title>404 - Page Not Found</title> </Head>
-  <h1 className={utilStyles.headingXl} >404 - Page Not Found</h1>;
-  <a href="https://rafemedia.com/">Go To Rafe Media</a>
-  </Layout>  
-);
-  }
+      <h1 className={utilStyles.headingXl} >404 - Page Not Found</h1>
+      <a href="https://rafemedia.com/">Go To Rafe Media</a>
+     <div>
+      <Link className={styles.backToHome} href="/">← Back to Blog</Link>
+      </div>
+    </div>
+  );
+}
